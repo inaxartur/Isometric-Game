180e5275eb6bf984ed6d2bb6721944c2b4de9dbe
adameczek <adam-sawicki-stanul@proton.me>
2025-12-10 11:42:37 +0100

    add enemy code 1st stage

--- Scripts/VirtualPlayer.gd
+++ Scripts/VirtualPlayer.gd
@@ -2,30 +2,50 @@ extends Player
 class_name VirtualPlayer
 
 @export var nav : NavigationAgent2D
+@export var tracking_player : CharacterBody2D
 ## Ready function ##
 func _ready() -> void:
 	super._ready()
 	assert(nav, "No navigation agent provided!")
-	
-	
+	tracking_player = find_player()
+	actor_setup.call_deferred()
 
+func find_player():
+	return get_tree().get_nodes_in_group("players").front()
+		
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
+	tracking_player = find_player()
 	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(player.position)
+	if tracking_player:
+		set_movement_target(tracking_player.position)
 
 func set_movement_target(movement_target: Vector2):
 	nav.target_position = movement_target
-	
+
 ## physics process function
 func _physics_process(delta: float) -> void:
 	staminaRegen(delta)
+
+	# Update the player position
+	if tracking_player:
+		set_movement_target(tracking_player.position)
+
+	# If we're at the target, stop
+	if nav.is_navigation_finished():
+		return
+
+	# Get pathfinding information
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = nav.get_next_path_position()
+
+	# Calculate the new velocity
+	direction = current_agent_position.direction_to(next_path_position)
 	movementHandler(delta)
 	character.move_and_slide()
 
 func staminaRegen(delta: float) -> void:
-	set_stamina_label()
 	if (stamina < max_stamina) and !is_dodging and !is_attacking:
 		stamina += (delta * stamina_regen_rate)
 
@@ -44,12 +64,6 @@ func attack() -> void:
 		stamina -= attack_stamina_cost
 		attack_cooldown.start()
 		is_attacking = true
-
-func inputHandler() -> void:
-	if Input.is_action_just_pressed("dodge") and !is_dodging:
-		dodge()
-	if Input.is_action_just_pressed("attack") and !is_attacking and !is_dodging:
-		attack()
 
 func movementHandler(delta: float) -> void:
 	if is_dodging:
